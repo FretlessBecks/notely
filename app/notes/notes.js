b414ffcc656a60f92857d6e2b400c6bd7e40cf13
Dave Strus <dave@davestrus.com>
2015-09-29 17:00:58 -0400

    Add textAngular for WYSIWYG editing.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,6 +1,7 @@
 (function() {
   angular.module('notely.notes', [
-    'ui.router'
+    'ui.router',
+    'textAngular'
   ])
   .config(notesConfig);
 
@@ -35,12 +36,14 @@
   NotesFormController['$inject'] = ['$scope', '$state', 'notes'];
   function NotesFormController($scope, $state, notes) {
     $scope.note = notes.findById($state.params.noteId);
+
     $scope.buttonText = function() {
       if ($scope.note.id) {
         return 'Save Changes';
       }
       return 'Save';
     }
+
     $scope.save = function() {
       if ($scope.note.id) {
         notes.update($scope.note).success(function(data) {
@@ -51,6 +54,7 @@
         notes.create($scope.note);
       }
     }
+
     $scope.delete = function() {
       notes.delete($scope.note)
       .success(function() {
