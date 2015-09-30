ff5d95e01c403f559dd488bcd3b4622464fd8474
Dave Strus <dave@davestrus.com>
2015-09-30 09:58:10 -0400

    Add a 'login' state.

---
+++ app/login/login.js
@@ -0,0 +1,16 @@
+(function() {
+  angular.module('notely.login', [
+    'ui.router'
+  ])
+  .config(loginConfig);
+
+  loginConfig['$inject'] = ['$stateProvider'];
+  function loginConfig($stateProvider) {
+    $stateProvider
+
+      .state('login', {
+        url: '/login',
+        templateUrl: 'login/login.html'
+      });
+  }
+})();
