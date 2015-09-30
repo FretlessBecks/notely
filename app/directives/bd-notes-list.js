85e5449a803d24a9c232854be9aae8bf78009600
Dave Strus <dave@davestrus.com>
2015-09-30 15:31:53 -0400

    Extract notes sidebar into a custom directive.

---
+++ app/directives/bd-notes-list.js
@@ -0,0 +1,16 @@
+angular.module('notely')
+.directive('bdNotesList', function() {
+  return {
+    restrict: 'EA',
+    replace: true,
+    scope: {},
+    templateUrl: '/notes/notes-list.html',
+    controller: NotesListController,
+    controllerAs: 'ctrl'
+  };
+
+  NotesListController['$inject'] = ['notes'];
+  function NotesListController(notes) {
+    this.notes = notes.all();
+  }
+});
